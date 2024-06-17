1c78809554704d567edcf91f1c56618b704d966e
EC2 Default User <ec2-user@ip-172-31-16-178.ec2.internal>
2024-06-17 10:00:11 +0000

    without variables

--- website.sh
+++ website.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-$URL=https://www.tooplate.com/zip-templates/2118_chilling_cafe
 sudo yum install wget net-tools unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 mkdir -p webfiles
 cd webfiles
-sudo wget $URL.zip
+sudo wget https://www.tooplate.com/zip-templates/2118_chilling_cafe.zip
 sudo unzip -o 2118_chilling_cafe.zip
 sudo rm -rf /var/www/html/*
 sudo cp -r 2118_chilling_cafe/* /var/www/html/
